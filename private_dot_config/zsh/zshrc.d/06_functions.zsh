95582840606a15f5034d6cd9b45df1087e968a95
toratako <125174203+toratako@users.noreply.github.com>
2026-04-08 23:41:26 +0900

    import from private

--- private_dot_config/zsh/zshrc.d/06_functions.zsh
+++ private_dot_config/zsh/zshrc.d/06_functions.zsh
@@ -40,3 +40,50 @@ gg() {
     local repo_url=$(echo "$url" | sed -E 's|(https://github\.com/[^/]+/[^/]+).*|\1|')
     ghq get "$repo_url"
 }
+
+
+wcx() {
+    local total_l=0 total_w=0 total_m=0 total_c=0
+    
+    printf "%10s %10s %10s %10s  %s\n" "Lines" "Words" "Chars" "Bytes" "Target"
+    echo "----------------------------------------------------------------------"
+
+    _do_wc() {
+        local input_file="$1"
+        local res
+
+        if [[ -z "$input_file" ]]; then
+            res=$(wc -l -w -m -c)
+        else
+            res=$(wc -l -w -m -c "$input_file" 2>/dev/null)
+        fi
+
+        [[ -z "$res" ]] && return
+
+        local stats=($(echo $res))
+        local l=${stats[1]}
+        local w=${stats[2]}
+        local m=${stats[3]}
+        local c=${stats[4]}
+        local label=${stats[5]:--}
+
+        printf "%10d %10d %10d %10d  %s\n" $l $w $m $c "$label"
+        
+        total_l=$((total_l + l))
+        total_w=$((total_w + w))
+        total_m=$((total_m + m))
+        total_c=$((total_c + c))
+    }
+
+    if [ -t 0 ]; then
+        [[ $# -eq 0 ]] && { echo "Usage: wcx [file ...]"; return; }
+        for f in "$@"; do _do_wc "$f"; done
+        
+        if [ $# -gt 1 ]; then
+            echo "----------------------------------------------------------------------"
+            printf "\e[1;32m%10d %10d %10d %10d  %s\e[m\n" $total_l $total_w $total_m $total_c "TOTAL"
+        fi
+    else
+        _do_wc
+    fi
+}
